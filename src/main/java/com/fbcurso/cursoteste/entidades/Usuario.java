a6a8b366527ed73992a86d26ce2d6c48c03dba50
isac1708 <isac.figueredo@gmail.com>
2023-05-16 14:45:31 -0300

    Criação de Banco de Dados

--- src/main/java/com/fbcurso/cursoteste/entidades/Usuario.java
+++ src/main/java/com/fbcurso/cursoteste/entidades/Usuario.java
@@ -2,8 +2,19 @@ package com.fbcurso.cursoteste.entidades;
 
 import java.io.Serializable;
 
-public class Usuario implements Serializable {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name="tb_user")
+public class Usuario implements Serializable {
+    private static final long serialVersionUID=1L; 
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String nome;
     private String email;
