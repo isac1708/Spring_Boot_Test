b549a23c00307f1a6a5be2a2c65b2199d0ec8170
isac1708 <isac.figueredo@gmail.com>
2023-05-15 23:50:15 -0300

    teste de commit

---
+++ src/main/java/com/fbcurso/cursoteste/CursotesteApplication.java
@@ -0,0 +1,13 @@
+package com.fbcurso.cursoteste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursotesteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursotesteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fbcurso/cursoteste/CursotesteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fbcurso.cursoteste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursotesteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
